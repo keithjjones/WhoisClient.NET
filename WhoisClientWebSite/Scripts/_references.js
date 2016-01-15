ab19ba9113d7606fa15ddc6c66e3e84fef3128f2
Keith J. Jones <keith@keithjjones.com>
2016-01-15 10:45:14 -0500

    Updated most packages.

--- WhoisClientWebSite/Scripts/_references.js
+++ WhoisClientWebSite/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-1.8.2.js" />
+﻿/// <reference path="jquery-2.2.0.js" />
 /// <reference path="jquery-ui-1.8.11.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="knockout-2.0.0.debug.js" />
